2beca4efd15955d54f27e37a4e694505c2ab8e97
Mudit Dahiya <mdtheroxtar@gmail.com>
2022-03-08 21:35:31 -0500

    port number in console

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("This is back end root");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port 3000");
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
