d60f8d9339a9f24fcdaa98fa55e5e73f6da14cbc
Mudit Dahiya <mdtheroxtar@gmail.com>
2022-03-08 21:23:13 -0500

    trying 1

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const cors = require("cors");
 //SERVER
 const express = require("express");
 const app = express();
+let port = process.env.PORT || 3001;
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(port, () => {
   var today = new Date();
   var time =
     (today.getHours() % 12) +
