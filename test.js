e95eec2d87f1adccae62401e405c46427c941730
Mudit Dahiya <mdtheroxtar@gmail.com>
2022-03-08 22:33:17 -0500

    added all requests to test

--- test.js
+++ test.js
@@ -14,16 +14,6 @@ let port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("This is back end root");
-});
-
-//START SERVER
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-  console.log(process.env.DATABASE_URL);
-});
-
 //DATABASE
 const { Client } = require("pg");
 const res = require("express/lib/response");
@@ -36,3 +26,92 @@ const client = new Client({
 });
 
 client.connect();
+
+//REQUESTS
+app.get("/", (req, res) => {
+  res.send("This is back end root");
+});
+
+//USERS
+app.get("/allusers", async (req, res) => {
+  const allUsers = await client.query("SELECT * FROM users");
+  res.json(allUsers.rows);
+});
+
+app.post("/newuser", (req, resp) => {
+  console.log(req.body);
+  client.query(
+    `INSERT INTO users (email, name, hash) VALUES ('${req.body.email}', '${req.body.name}', '${req.body.hash}')`,
+    (err, res) => {
+      if (err) {
+        console.log("pg returned an error");
+        resp.send("Couldnt add user");
+      }
+      if (res) {
+        resp.send("Added new user");
+      }
+    }
+  );
+});
+
+app.put("/deleteusers", (req, res) => {
+  client.query("DELETE FROM users");
+  res.send("Deleted all users");
+});
+
+app.put("/signin", async (req, resp) => {
+  try {
+    const ans = await client.query(
+      `SELECT hash FROM users WHERE email = '${req.body.email}'`
+    );
+
+    if (req.body.hash == ans.rows[0].hash) {
+      resp.send("Successful login");
+    } else {
+      resp.send("Check password");
+    }
+  } catch (err) {
+    resp.send("No such user found");
+  }
+});
+
+//POSTS
+app.post("/newpost", (req, resp) => {
+  console.log(req.body);
+  client.query(
+    `INSERT INTO posts (author, title, content, tags) VALUES ('${req.body.author}', '${req.body.title}', '${req.body.content}', '${req.body.tags}')`,
+    (err, res) => {
+      if (err) {
+        console.log("pg returned an error");
+        console.log(err);
+        resp.send("Couldnt add post");
+      }
+      if (res) {
+        resp.send("Added new post");
+      }
+    }
+  );
+});
+
+app.get("/allposts", async (req, res) => {
+  const allUsers = await client.query("SELECT * FROM posts ORDER BY date desc");
+  res.json(allUsers.rows);
+});
+
+app.put("/deleteposts", (req, res) => {
+  client.query("DELETE FROM posts");
+  res.send("Deleted all posts");
+});
+
+app.put("/deletetoppost", (req, res) => {
+  client.query(
+    "DELETE FROM posts WHERE id = (SELECT id FROM posts ORDER BY date desc LIMIT 1)"
+  );
+  res.send("Deleted top post");
+});
+
+//START SERVER
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  console.log(process.env.DATABASE_URL);
+});
