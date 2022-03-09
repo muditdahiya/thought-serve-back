6caad32cc7a94266617b54ba742490aad6b5d9d9
Mudit Dahiya <mdtheroxtar@gmail.com>
2022-03-08 21:18:25 -0500

    added alternate port

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send("This is back end root");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
